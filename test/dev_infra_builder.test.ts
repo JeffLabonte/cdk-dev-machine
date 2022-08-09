827e95017d78a4af8d6ce00ad103495bcfcac2ff
Jean-Francois Labonte <jflabonte@pop-os.localdomain>
2022-08-08 20:51:51 -0400

    Initial commit

---
+++ test/dev_infra_builder.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DevInfraBuilder from '../lib/dev_infra_builder-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dev_infra_builder-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DevInfraBuilder.DevInfraBuilderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
