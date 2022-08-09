e8edb7737eb9baf463c981aac5d62fe00a46ba50
Jeff Labonte <grimsleepless@protonmail.com>
2022-08-09 00:19:07 -0400

    Update user data

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -6,15 +6,23 @@ add-apt-repository -y ppa:neovim-ppa/stable
 
 apt update
 apt -y dist-upgrade
-apt install -y neofetch zsh neovim tmux mosh openssh-server mosh 
+apt install -y neofetch zsh neovim tmux mosh openssh-server mosh git
+
+echo "the_dev_machine" > /etc/hostname
 
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -fsSL https://tailscale.com/install.sh | sh
 
 systemctl enable --now sshd
 
 useradd -m -G sudo -s /bin/zsh jflabonte
-cp -r /home/ubuntu/.ssh /home/jflabonte/.ssh
+sudo cp -r /home/ubuntu/.ssh/* /home/jflabonte/.ssh/*
+sudo chown -R jflabonte: /home/jflabonte/.ssh
+
+sudo su - jflabonte
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim +PackerSync
 
 reboot
